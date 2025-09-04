e97d84155499a6cda91b1d960ab36fe1424a8cf3
lewinskie254 <machiulewis@gmail.com>
2025-09-05 00:22:48 +0300

    now done with the convolution class, time to think about the pooling layer as well

--- convclass.py
+++ convclass.py
@@ -1,45 +1,68 @@
 import numpy as np 
 
 class ConvLayer: 
-    def __init__(self, image, horizontal_filter = None, vertical_filter = None, stride=1, padding=0):
+    def __init__(self, image, filter='horizontal', stride=1, padding=0):
         self.input_dim_x = image.shape[0]   # height
         self.input_dim_y = image.shape[1]   # width
         self.no_of_channels = image.shape[2]  # channels
-        self.horizontal_edges_filter = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]]) if horizontal_filter == None else horizontal_filter 
-        self.vertical_edges_filter = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]]) if vertical_filter == None else vertical_filter
-        self.sharpen_edges_filter = np.array([[0, -1,0], [-1, 5, -1], [0, -1,  0]]) 
+
+        #Filters to use 
+        self.horizontal_edges_filter = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]) 
+        self.vertical_edges_filter = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]]) 
+        self.sharpen_edges_filter = np.array([[0, -1, 0], [-1, 5, -1], [0, -1,  0]]) 
+        self.emboss_filter = np.array([[-2, -1,  0], [-1,  1,  1], [ 0, 1, 2]])
+        self.gaussian_blur_filter = np.array([[1, 2, 1],[2, 4, 2],[1, 2, 1]]) / 16
+        self.box_blur_filter = np.array([[1, 1, 1],[1, 1, 1],[1, 1, 1]]) / 9
+
         self.filter_dim_x = self.horizontal_edges_filter.shape[0]
         self.filter_dim_y = self.horizontal_edges_filter.shape[1]
         self.stride = stride
         self.padding = padding 
         self.image = image 
 
+        match filter: 
+            case 'horizontal': 
+                self.filter = self.horizontal_edges_filter 
+            case 'vertical': 
+                self.filter =self.vertical_edges_filter 
+            case 'sharpen' :
+                self.filter = self.sharpen_edges_filter 
+            case 'emboss' :
+                self.filter = self.emboss_filter 
+            case 'gaussian' : 
+                self.filter = self.gaussian_blur_filter 
+            case _: 
+                self.filter =self.horizontal_edges_filter 
+            
+            
+
     def add_padding(self, image, padding, mode='constant', constant_values=0): 
         return np.pad(image, ((padding, padding), (padding, padding), (0, 0)), mode=mode, constant_values=constant_values)
 
         
 
-    def forward(self, x): 
-        image = self.add_padding(x, self.padding)
-        convolution_filters = [self.horizontal_edges_filter, self.vertical_edges_filter, self.sharpen_edges_filter] 
-        convolved_results = [[]]
-        for i in range(self.no_of_channels): 
-            filter_to_use = convolution_filters[i]
-            for j in range(self.input_dim_x): 
-                for k in range(self.input_dim_y): 
-                    if (j + filter_to_use.shape[0]) < self.input_dim_x and (k + filter_to_use.shape[1] < self.input_dim_y):
-                        sum = 0
-                        first_row = [image[j][k+0] * filter_to_use[0][0], image[j+1][k+0] * filter_to_use[1][0],image[j+2][k+0] * filter_to_use[2][0]]
-                        second_row = [image[j][k+1] * filter_to_use[0][1], image[j+1][k+1] * filter_to_use[1][1],image[j+1][k+1] * filter_to_use[2][1]]
-                        third_row = [image[j][k+2] * filter_to_use[0][2], image[j+2][k+2] * filter_to_use[0][0],image[j+2][k+2] * filter_to_use[2][2]]
-                        sum += np.sum(first_row) + np.sum(second_row) + np.sum(third_row)
-                    convolved_results[k].append(sum)
-                    j += 1
-                    if j + filter_to_use.shape[0] > self.input_dim_x:
-                        k += 1
-                    if k + filter_to_use.shape[1] > self.input_dim_y:
-                        break 
-        return convolved_results 
+    def forward(self, x):
+        image = self.add_padding(x, self.padding)  # padded image
+
+        # 3D filter spanning all channels
+        filter_to_use = np.stack([self.filter]*self.no_of_channels, axis=-1)
+        # shape now: (fh, fw, C)
+
+        filter_height, filter_width, _ = filter_to_use.shape
+        out_height = (self.input_dim_x + 2 * self.padding - filter_height) // self.stride + 1
+        out_width  = (self.input_dim_y + 2 * self.padding - filter_width) // self.stride + 1
+
+        convolved = np.zeros((out_height, out_width))
+
+        for i in range(0, out_height * self.stride, self.stride):
+            for j in range(0, out_width * self.stride, self.stride):
+                region = image[i:i+filter_height, j:j+filter_width, :]
+                convolved[i//self.stride, j//self.stride] = np.sum(region * filter_to_use)
+
+
+        return convolved
+
+
         
                         
 
